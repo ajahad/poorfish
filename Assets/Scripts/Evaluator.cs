68cfedc05e5c871e99cdd2d6e6a94747dedfb01a
Alvin <alvinliu2008@gmail.com>
2026-03-16 20:45:29 -0400

    PieceSquareTable + Evaluator .cs

--- Assets/Scripts/Evaluator.cs
+++ Assets/Scripts/Evaluator.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Evaluator : MonoBehaviour
+public static class Evaluator
 {
-    // Start is called before the first frame update
-    void Start()
+    // source chessprogramming wiki SEF for values
+    private const int ValPawn   = 100;
+    private const int ValKnight = 320;
+    private const int ValBishop = 330;
+    private const int ValRook   = 500;
+    private const int ValQueen  = 900;
+    private const int ValKing   = 0;
+
+    private const int EndgameThreshold = 1500;
+
+    public static bool IsEndgame(BoardState state)
     {
-        
+        int totalMaterial = 0;
+
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                Piece piece = state.GetPiece(col, row);
+                if (piece == null || piece.Type == PieceType.King) continue;
+
+                totalMaterial += GetMaterialValue(piece.Type);
+            }
+        }
+
+        return totalMaterial < EndgameThreshold;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static int Evaluate(BoardState state)
     {
-        
+        bool endgame = IsEndgame(state);
+        int score = 0;
+
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                Piece piece = state.GetPiece(col, row);
+                if (piece == null) continue;
+
+                int material = GetMaterialValue(piece.Type);
+                int pst = PieceSquareTables.GetPST(piece.Type, piece.Color, col, row, endgame);
+
+                if (piece.Color == PieceColor.White)
+                    score += material + pst;
+                else
+                    score -= material + pst;
+            }
+        }
+
+        return score;
+    }
+
+    private static int GetMaterialValue(PieceType type)
+    {
+        return type switch
+        {
+            PieceType.Pawn   => ValPawn,
+            PieceType.Knight => ValKnight,
+            PieceType.Bishop => ValBishop,
+            PieceType.Rook   => ValRook,
+            PieceType.Queen  => ValQueen,
+            PieceType.King   => ValKing,
+            _                => 0
+        };
     }
 }
